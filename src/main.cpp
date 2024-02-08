8493ec42f6c7195e24f9410794dfaf5082bb8595
Youssef-BMI <sarfouhiyoussef@gmail.com>
2024-02-08 11:18:33 +0000

    Gestion de la disponibilité

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main() {
   int capacite = 20;
   int choix;
   int duree;
+  float calculerCout(int duree, float coutH);
   do {
     cout << "****** Gestion d'une station Vélô ******" << endl << endl;
     cout << "-----------------------------------------" << endl;
@@ -19,23 +20,37 @@ int main() {
     cout << "4. Quitter l'application" << endl << endl;
     cin >> choix;
     cout << " Votre choix est,";
-    
+
     switch (choix) {
     case 1:
-      velosDispos --;
       std::cout << " Emprunter un vélô électrique\n";
+      if (velosDispos > 0) {
+        velosDispos--;
+      } else {
+        std::cout<< "\n";
+        std::cout << "Il n'y a plus de vélô électrique\n";
+        std::cout<< "\n";
+      }
+
       break;
 
     case 2:
-      velosDispos ++;
       std::cout << " Restituer un vélô électrique\n";
+      if (velosDispos < 20) {
+        velosDispos++;
+      } else {
+        std::cout<< "\n";
+        std::cout << "Aucune place n'est disponible\n";
+        std::cout<< "\n";
+      }
       break;
 
     case 3:
       std::cout << " Estimer le coût de la location\n";
-      std::cout << " Combien de temps souhaitez-vous conserver le vélo ? ( min )\n";
+      std::cout
+          << " Combien de temps souhaitez-vous conserver le vélo ? ( min )\n";
       std::cin >> duree;
-      std::cout << " Le prix serra de :" << (duree*coutHoraire)/60 << "€\n" ;
+      std::cout << " Le prix serra de :" << (duree * coutHoraire) / 60 << "€\n";
       break;
 
     case 4:
@@ -45,10 +60,11 @@ int main() {
       break;
 
     default:
+
       std::cout << " INCORECTE\n";
     }
     cout << "\n";
-  } while (choix != 4) ;
+  } while (choix != 4);
 
-      return 0;
+  return 0;
 }
