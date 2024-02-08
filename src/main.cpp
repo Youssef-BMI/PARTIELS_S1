79d96d29cd7b2d6ef89ba9e0cbdf3582f0d24ee3
Youssef-BMI <sarfouhiyoussef@gmail.com>
2024-02-08 10:21:17 +0000

    boucle de l'affichage

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,41 @@ int main() {
   int velosDispos = 20;
   int capacite = 20;
   int choix;
-  cout << "****** Gestion d'une station Vélô ******" << endl << endl;
-  cout << "-----------------------------------------" << endl;
-  cout << " Vélos disponibles : " << velosDispos << "\n";
-  cout << " Points d'attache disponibles : " << capacite - velosDispos << "\n";
-  cout << "-----------------------------------------" << endl << endl;
-  cout << "1. Emprunter un vélô électrique" << endl;
-  cout << "2. Restituer un vélô électrique" << endl;
-  cout << "3. Estimer le coût de la location" << endl;
-  cout << "4. Quitter l'application" << endl << endl;
-  cin >> choix;
-  cout << " Votre choix est,";
-  switch (choix) {
-  case 1:
-    std::cout << " Emprunter un vélô électrique\n";
-    break;
+  do {
+    cout << "****** Gestion d'une station Vélô ******" << endl << endl;
+    cout << "-----------------------------------------" << endl;
+    cout << " Vélos disponibles : " << velosDispos << "\n";
+    cout << " Points d'attache disponibles : " << capacite - velosDispos
+         << "\n";
+    cout << "-----------------------------------------" << endl << endl;
+    cout << "1. Emprunter un vélô électrique" << endl;
+    cout << "2. Restituer un vélô électrique" << endl;
+    cout << "3. Estimer le coût de la location" << endl;
+    cout << "4. Quitter l'application" << endl << endl;
+    cin >> choix;
+    cout << " Votre choix est,";
+    switch (choix) {
+    case 1:
+      std::cout << " Emprunter un vélô électrique\n";
+      break;
 
-  case 2:
-    std::cout << " Restituer un vélô électrique\n";
-    break;
+    case 2:
+      std::cout << " Restituer un vélô électrique\n";
+      break;
 
-  case 3:
-    std::cout << " Estimer le coût de la location\n";
-    break;
+    case 3:
+      std::cout << " Estimer le coût de la location\n";
+      break;
 
-  case 4:
-    std::cout << " Quitter l'application\n";
-    break;
+    case 4:
+      std::cout << " Quitter l'application\n";
+      break;
 
-  default:
-    std::cout << " INCORECTE\n";
-  }
-  return 0;
+    default:
+      std::cout << " INCORECTE\n";
+    }
+    cout << "\n";
+  } while (choix != 4) ;
+
+      return 0;
 }
